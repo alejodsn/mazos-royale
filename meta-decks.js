f6cff55ebbc95898e9d7f7299f7ea778be9d4aef
alejodsn <alejodsn@gmail.com>
2026-06-18 21:20:41 -0500

    Add files via upload

---
+++ meta-decks.js
@@ -0,0 +1,197 @@
+export const metaDecks = [
+  {
+    name: "2.6 Hog Cycle (2 Evos)",
+    cards: [
+      { id: 26000021, key: "hog-rider", elixir: 4, reqEvo: false },
+      { id: 26000014, key: "musketeer", elixir: 4, reqEvo: false },
+      { id: 26000038, key: "ice-golem", elixir: 2, reqEvo: false },
+      { id: 26000010, key: "skeletons", elixir: 1, reqEvo: true },
+      { id: 26000030, key: "ice-spirit", elixir: 1, reqEvo: true },
+      { id: 27000000, key: "cannon", elixir: 3, reqEvo: false },
+      { id: 28000000, key: "fireball", elixir: 4, reqEvo: false },
+      { id: 28000011, key: "the-log", elixir: 2, reqEvo: false }
+    ]
+  },
+  {
+    name: "Log Bait Clásico (2 Evos)",
+    cards: [
+      { id: 28000004, key: "goblin-barrel", elixir: 3, reqEvo: true },
+      { id: 26000026, key: "princess", elixir: 3, reqEvo: false },
+      { id: 26000041, key: "goblin-gang", elixir: 3, reqEvo: false },
+      { id: 26000000, key: "knight", elixir: 3, reqEvo: true },
+      { id: 27000003, key: "inferno-tower", elixir: 5, reqEvo: false },
+      { id: 28000011, key: "the-log", elixir: 2, reqEvo: false },
+      { id: 28000003, key: "rocket", elixir: 6, reqEvo: false },
+      { id: 26000030, key: "ice-spirit", elixir: 1, reqEvo: false }
+    ]
+  },
+  {
+    name: "X-Bow 3.0 (2 Evos)",
+    cards: [
+      { id: 27000008, key: "x-bow", elixir: 6, reqEvo: false },
+      { id: 26000000, key: "knight", elixir: 3, reqEvo: true },
+      { id: 26000001, key: "archers", elixir: 3, reqEvo: true },
+      { id: 26000010, key: "skeletons", elixir: 1, reqEvo: false },
+      { id: 26000084, key: "electro-spirit", elixir: 1, reqEvo: false },
+      { id: 27000006, key: "tesla", elixir: 4, reqEvo: false },
+      { id: 28000011, key: "the-log", elixir: 2, reqEvo: false },
+      { id: 28000000, key: "fireball", elixir: 4, reqEvo: false }
+    ]
+  },
+  {
+    name: "Miner Wall Breakers (2 Evos)",
+    cards: [
+      { id: 26000032, key: "miner", elixir: 3, reqEvo: false },
+      { id: 26000058, key: "wall-breakers", elixir: 2, reqEvo: true },
+      { id: 26000049, key: "bats", elixir: 2, reqEvo: false },
+      { id: 26000013, key: "bomber", elixir: 2, reqEvo: true },
+      { id: 26000093, key: "little-prince", elixir: 3, reqEvo: false },
+      { id: 27000004, key: "bomb-tower", elixir: 4, reqEvo: false },
+      { id: 28000011, key: "the-log", elixir: 2, reqEvo: false },
+      { id: 28000009, key: "poison", elixir: 4, reqEvo: false }
+    ]
+  },
+  {
+    name: "Royal Giant (2 Evos)",
+    cards: [
+      { id: 26000024, key: "royal-giant", elixir: 6, reqEvo: true },
+      { id: 26000064, key: "firecracker", elixir: 3, reqEvo: true },
+      { id: 26000061, key: "fisherman", elixir: 3, reqEvo: false },
+      { id: 26000050, key: "royal-ghost", elixir: 3, reqEvo: false },
+      { id: 26000069, key: "skeleton-king", elixir: 4, reqEvo: false },
+      { id: 28000011, key: "the-log", elixir: 2, reqEvo: false },
+      { id: 26000052, key: "zappies", elixir: 4, reqEvo: false },
+      { id: 28000000, key: "fireball", elixir: 4, reqEvo: false }
+    ]
+  },
+  {
+    name: "Golem Night Witch (2 Evos)",
+    cards: [
+      { id: 26000009, key: "golem", elixir: 8, reqEvo: false },
+      { id: 26000039, key: "night-witch", elixir: 4, reqEvo: false },
+      { id: 26000035, key: "lumberjack", elixir: 4, reqEvo: false },
+      { id: 26000063, key: "electro-dragon", elixir: 5, reqEvo: false },
+      { id: 28000012, key: "tornado", elixir: 3, reqEvo: false },
+      { id: 26000013, key: "bomber", elixir: 2, reqEvo: true },
+      { id: 28000008, key: "zap", elixir: 2, reqEvo: true },
+      { id: 27000007, key: "elixir-collector", elixir: 6, reqEvo: false }
+    ]
+  },
+  {
+    name: "Lavaloon (2 Evos)",
+    cards: [
+      { id: 26000029, key: "lava-hound", elixir: 7, reqEvo: false },
+      { id: 26000006, key: "balloon", elixir: 5, reqEvo: false },
+      { id: 26000080, key: "skeleton-dragons", elixir: 4, reqEvo: false },
+      { id: 26000037, key: "inferno-dragon", elixir: 4, reqEvo: false },
+      { id: 27000009, key: "tombstone", elixir: 3, reqEvo: false },
+      { id: 26000011, key: "valkyrie", elixir: 4, reqEvo: true },
+      { id: 28000008, key: "zap", elixir: 2, reqEvo: true },
+      { id: 28000000, key: "fireball", elixir: 4, reqEvo: false }
+    ]
+  },
+  {
+    name: "Pekka Bridge Spam (2 Evos)",
+    cards: [
+      { id: 26000004, key: "pekka", elixir: 7, reqEvo: false },
+      { id: 26000036, key: "battle-ram", elixir: 4, reqEvo: true },
+      { id: 26000046, key: "bandit", elixir: 3, reqEvo: false },
+      { id: 26000050, key: "royal-ghost", elixir: 3, reqEvo: false },
+      { id: 26000062, key: "magic-archer", elixir: 4, reqEvo: false },
+      { id: 26000042, key: "electro-wizard", elixir: 4, reqEvo: false },
+      { id: 28000009, key: "poison", elixir: 4, reqEvo: false },
+      { id: 28000008, key: "zap", elixir: 2, reqEvo: true }
+    ]
+  },
+  {
+    name: "Splashyard (2 Evos)",
+    cards: [
+      { id: 28000010, key: "graveyard", elixir: 5, reqEvo: false },
+      { id: 28000009, key: "poison", elixir: 4, reqEvo: false },
+      { id: 26000000, key: "knight", elixir: 3, reqEvo: true },
+      { id: 26000010, key: "skeletons", elixir: 1, reqEvo: true },
+      { id: 26000015, key: "baby-dragon", elixir: 4, reqEvo: false },
+      { id: 26000023, key: "ice-wizard", elixir: 3, reqEvo: false },
+      { id: 27000009, key: "tombstone", elixir: 3, reqEvo: false },
+      { id: 28000012, key: "tornado", elixir: 3, reqEvo: false }
+    ]
+  },
+  {
+    name: "Royal Recruits Hogs (2 Evos)",
+    cards: [
+      { id: 26000047, key: "royal-recruits", elixir: 7, reqEvo: true },
+      { id: 26000059, key: "royal-hogs", elixir: 5, reqEvo: false },
+      { id: 26000064, key: "firecracker", elixir: 3, reqEvo: true },
+      { id: 26000057, key: "flying-machine", elixir: 4, reqEvo: false },
+      { id: 26000052, key: "zappies", elixir: 4, reqEvo: false },
+      { id: 27000001, key: "goblin-hut", elixir: 5, reqEvo: false },
+      { id: 28000018, key: "royal-delivery", elixir: 3, reqEvo: false },
+      { id: 28000014, key: "earthquake", elixir: 3, reqEvo: false }
+    ]
+  },
+  {
+    name: "Mega Knight Bait",
+    cards: [
+      { id: 26000055, key: "mega-knight", elixir: 7, reqEvo: false },
+      { id: 26000058, key: "wall-breakers", elixir: 2, reqEvo: false },
+      { id: 26000032, key: "miner", elixir: 3, reqEvo: false },
+      { id: 26000049, key: "bats", elixir: 2, reqEvo: true },
+      { id: 26000019, key: "spear-goblins", elixir: 2, reqEvo: false },
+      { id: 26000041, key: "goblin-gang", elixir: 3, reqEvo: false },
+      { id: 26000016, key: "prince", elixir: 5, reqEvo: false },
+      { id: 28000008, key: "zap", elixir: 2, reqEvo: false }
+    ]
+  },
+  {
+    name: "Goblin Giant Sparky (2 Evos)",
+    cards: [
+      { id: 26000060, key: "goblin-giant", elixir: 6, reqEvo: false },
+      { id: 26000033, key: "sparky", elixir: 6, reqEvo: false },
+      { id: 26000018, key: "mini-pekka", elixir: 4, reqEvo: false },
+      { id: 26000027, key: "dark-prince", elixir: 4, reqEvo: false },
+      { id: 26000042, key: "electro-wizard", elixir: 4, reqEvo: false },
+      { id: 26000000, key: "knight", elixir: 3, reqEvo: true },
+      { id: 28000008, key: "zap", elixir: 2, reqEvo: true },
+      { id: 28000002, key: "rage", elixir: 2, reqEvo: false }
+    ]
+  },
+  {
+    name: "Elixir Golem",
+    cards: [
+      { id: 26000067, key: "elixir-golem", elixir: 3, reqEvo: false },
+      { id: 26000068, key: "battle-healer", elixir: 4, reqEvo: false },
+      { id: 26000063, key: "electro-dragon", elixir: 5, reqEvo: false },
+      { id: 26000037, key: "inferno-dragon", elixir: 4, reqEvo: false },
+      { id: 26000039, key: "night-witch", elixir: 4, reqEvo: false },
+      { id: 26000013, key: "bomber", elixir: 2, reqEvo: true },
+      { id: 28000008, key: "zap", elixir: 2, reqEvo: false },
+      { id: 28000012, key: "tornado", elixir: 3, reqEvo: false }
+    ]
+  },
+  {
+    name: "Drill Poison",
+    cards: [
+      { id: 27000013, key: "goblin-drill", elixir: 4, reqEvo: false },
+      { id: 28000009, key: "poison", elixir: 4, reqEvo: false },
+      { id: 26000000, key: "knight", elixir: 3, reqEvo: true },
+      { id: 26000013, key: "bomber", elixir: 2, reqEvo: false },
+      { id: 26000093, key: "little-prince", elixir: 3, reqEvo: false },
+      { id: 26000010, key: "skeletons", elixir: 1, reqEvo: false },
+      { id: 27000006, key: "tesla", elixir: 4, reqEvo: false },
+      { id: 28000011, key: "the-log", elixir: 2, reqEvo: false }
+    ]
+  },
+  {
+    name: "Mortar Bait",
+    cards: [
+      { id: 27000002, key: "mortar", elixir: 4, reqEvo: true },
+      { id: 26000032, key: "miner", elixir: 3, reqEvo: false },
+      { id: 26000069, key: "skeleton-king", elixir: 4, reqEvo: false },
+      { id: 26000049, key: "bats", elixir: 2, reqEvo: false },
+      { id: 26000019, key: "spear-goblins", elixir: 2, reqEvo: false },
+      { id: 26000022, key: "minion-horde", elixir: 5, reqEvo: false },
+      { id: 26000041, key: "goblin-gang", elixir: 3, reqEvo: false },
+      { id: 28000011, key: "the-log", elixir: 2, reqEvo: false }
+    ]
+  }
+];
